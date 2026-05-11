d1183993414d7ced4e912809070d2d682cca240f
Zuza <zuz.kaczmarczyk.zk@email.com>
2026-05-11 18:36:09 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Home from './view/Home';
+
+export default function App() {
+  return <Home />;
+}
